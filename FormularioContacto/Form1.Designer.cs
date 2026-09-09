65d92197fc11ce721683de3108ff5225531369b3
icampos7-beep <icampos7@ucol.mx>
2026-09-08 22:18:15 -0600

    programa listo

--- FormularioContacto/Form1.Designer.cs
+++ FormularioContacto/Form1.Designer.cs
@@ -32,9 +32,9 @@
             this.lblNombre = new System.Windows.Forms.Label();
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.grpMotivo = new System.Windows.Forms.GroupBox();
-            this.radConsulta = new System.Windows.Forms.RadioButton();
-            this.radQueja = new System.Windows.Forms.RadioButton();
             this.radSugerencia = new System.Windows.Forms.RadioButton();
+            this.radQueja = new System.Windows.Forms.RadioButton();
+            this.radConsulta = new System.Windows.Forms.RadioButton();
             this.chkRespuesta = new System.Windows.Forms.CheckBox();
             this.btnEnviar = new System.Windows.Forms.Button();
             this.lblConf = new System.Windows.Forms.Label();
@@ -47,9 +47,10 @@
             // 
             this.txtFormulario.AutoSize = true;
             this.txtFormulario.Font = new System.Drawing.Font("NSimSun", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFormulario.Location = new System.Drawing.Point(125, 22);
+            this.txtFormulario.Location = new System.Drawing.Point(166, 9);
+            this.txtFormulario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.txtFormulario.Name = "txtFormulario";
-            this.txtFormulario.Size = new System.Drawing.Size(365, 29);
+            this.txtFormulario.Size = new System.Drawing.Size(457, 37);
             this.txtFormulario.TabIndex = 0;
             this.txtFormulario.Text = "Formulario de Contacto";
             // 
@@ -57,17 +58,19 @@
             // 
             this.lblNombre.AutoSize = true;
             this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombre.Location = new System.Drawing.Point(55, 74);
+            this.lblNombre.Location = new System.Drawing.Point(60, 67);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(84, 24);
+            this.lblNombre.Size = new System.Drawing.Size(107, 29);
             this.lblNombre.TabIndex = 1;
             this.lblNombre.Text = "Nombre:";
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(145, 74);
+            this.txtNombre.Location = new System.Drawing.Point(173, 74);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(416, 20);
+            this.txtNombre.Size = new System.Drawing.Size(553, 22);
             this.txtNombre.TabIndex = 2;
             this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
             // 
@@ -77,63 +80,71 @@
             this.grpMotivo.Controls.Add(this.radQueja);
             this.grpMotivo.Controls.Add(this.radConsulta);
             this.grpMotivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grpMotivo.Location = new System.Drawing.Point(59, 110);
+            this.grpMotivo.Location = new System.Drawing.Point(65, 123);
+            this.grpMotivo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grpMotivo.Name = "grpMotivo";
-            this.grpMotivo.Size = new System.Drawing.Size(245, 146);
+            this.grpMotivo.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpMotivo.Size = new System.Drawing.Size(327, 180);
             this.grpMotivo.TabIndex = 3;
             this.grpMotivo.TabStop = false;
             this.grpMotivo.Text = "Motivo de Contacto";
             // 
-            // radConsulta
-            // 
-            this.radConsulta.AutoSize = true;
-            this.radConsulta.Location = new System.Drawing.Point(17, 36);
-            this.radConsulta.Name = "radConsulta";
-            this.radConsulta.Size = new System.Drawing.Size(90, 28);
-            this.radConsulta.TabIndex = 0;
-            this.radConsulta.TabStop = true;
-            this.radConsulta.Text = "Cosulta";
-            this.radConsulta.UseVisualStyleBackColor = true;
-            // 
-            // radQueja
-            // 
-            this.radQueja.AutoSize = true;
-            this.radQueja.Location = new System.Drawing.Point(17, 70);
-            this.radQueja.Name = "radQueja";
-            this.radQueja.Size = new System.Drawing.Size(79, 28);
-            this.radQueja.TabIndex = 1;
-            this.radQueja.TabStop = true;
-            this.radQueja.Text = "Queja";
-            this.radQueja.UseVisualStyleBackColor = true;
-            // 
             // radSugerencia
             // 
             this.radSugerencia.AutoSize = true;
-            this.radSugerencia.Location = new System.Drawing.Point(17, 104);
+            this.radSugerencia.Location = new System.Drawing.Point(23, 128);
+            this.radSugerencia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radSugerencia.Name = "radSugerencia";
-            this.radSugerencia.Size = new System.Drawing.Size(125, 28);
+            this.radSugerencia.Size = new System.Drawing.Size(157, 33);
             this.radSugerencia.TabIndex = 2;
             this.radSugerencia.TabStop = true;
             this.radSugerencia.Text = "Sugerencia";
             this.radSugerencia.UseVisualStyleBackColor = true;
             // 
+            // radQueja
+            // 
+            this.radQueja.AutoSize = true;
+            this.radQueja.Location = new System.Drawing.Point(23, 86);
+            this.radQueja.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radQueja.Name = "radQueja";
+            this.radQueja.Size = new System.Drawing.Size(99, 33);
+            this.radQueja.TabIndex = 1;
+            this.radQueja.TabStop = true;
+            this.radQueja.Text = "Queja";
+            this.radQueja.UseVisualStyleBackColor = true;
+            // 
+            // radConsulta
+            // 
+            this.radConsulta.AutoSize = true;
+            this.radConsulta.Location = new System.Drawing.Point(23, 44);
+            this.radConsulta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radConsulta.Name = "radConsulta";
+            this.radConsulta.Size = new System.Drawing.Size(115, 33);
+            this.radConsulta.TabIndex = 0;
+            this.radConsulta.TabStop = true;
+            this.radConsulta.Text = "Cosulta";
+            this.radConsulta.UseVisualStyleBackColor = true;
+            // 
             // chkRespuesta
             // 
             this.chkRespuesta.AutoSize = true;
             this.chkRespuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkRespuesta.Location = new System.Drawing.Point(59, 271);
+            this.chkRespuesta.Location = new System.Drawing.Point(65, 322);
+            this.chkRespuesta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkRespuesta.Name = "chkRespuesta";
-            this.chkRespuesta.Size = new System.Drawing.Size(196, 24);
+            this.chkRespuesta.Size = new System.Drawing.Size(239, 29);
             this.chkRespuesta.TabIndex = 4;
             this.chkRespuesta.Text = "Deseo recibir respuesta";
             this.chkRespuesta.UseVisualStyleBackColor = true;
+            this.chkRespuesta.CheckedChanged += new System.EventHandler(this.chkRespuesta_CheckedChanged);
             // 
             // btnEnviar
             // 
             this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEnviar.Location = new System.Drawing.Point(59, 301);
+            this.btnEnviar.Location = new System.Drawing.Point(101, 368);
+            this.btnEnviar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEnviar.Name = "btnEnviar";
-            this.btnEnviar.Size = new System.Drawing.Size(119, 36);
+            this.btnEnviar.Size = new System.Drawing.Size(159, 44);
             this.btnEnviar.TabIndex = 5;
             this.btnEnviar.Text = "Enviar";
             this.btnEnviar.UseVisualStyleBackColor = true;
@@ -143,20 +154,22 @@
             // 
             this.lblConf.AutoSize = true;
             this.lblConf.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblConf.Location = new System.Drawing.Point(46, 353);
+            this.lblConf.Location = new System.Drawing.Point(61, 432);
+            this.lblConf.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblConf.Name = "lblConf";
-            this.lblConf.Size = new System.Drawing.Size(101, 18);
+            this.lblConf.Size = new System.Drawing.Size(126, 24);
             this.lblConf.TabIndex = 6;
             this.lblConf.Text = "Confirmación:";
             this.lblConf.Click += new System.EventHandler(this.lblConf_Click);
             // 
             // txtConf
             // 
-            this.txtConf.Location = new System.Drawing.Point(49, 374);
+            this.txtConf.Location = new System.Drawing.Point(34, 460);
+            this.txtConf.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtConf.Multiline = true;
             this.txtConf.Name = "txtConf";
             this.txtConf.ReadOnly = true;
-            this.txtConf.Size = new System.Drawing.Size(523, 58);
+            this.txtConf.Size = new System.Drawing.Size(719, 143);
             this.txtConf.TabIndex = 7;
             this.txtConf.TextChanged += new System.EventHandler(this.txtConf_TextChanged);
             // 
@@ -164,18 +177,20 @@
             // 
             this.linkPolitica.AutoSize = true;
             this.linkPolitica.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkPolitica.Location = new System.Drawing.Point(207, 447);
+            this.linkPolitica.Location = new System.Drawing.Point(267, 624);
+            this.linkPolitica.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkPolitica.Name = "linkPolitica";
-            this.linkPolitica.Size = new System.Drawing.Size(184, 20);
+            this.linkPolitica.Size = new System.Drawing.Size(229, 25);
             this.linkPolitica.TabIndex = 8;
             this.linkPolitica.TabStop = true;
             this.linkPolitica.Text = "Ver politica de privacidad";
+            this.linkPolitica.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkPolitica_LinkClicked);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(620, 488);
+            this.ClientSize = new System.Drawing.Size(786, 675);
             this.Controls.Add(this.linkPolitica);
             this.Controls.Add(this.txtConf);
             this.Controls.Add(this.lblConf);
@@ -185,6 +200,7 @@
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.txtFormulario);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Forulario de contacto";
             this.grpMotivo.ResumeLayout(false);
--- FormularioContacto/Form1.cs
+++ FormularioContacto/Form1.cs
@@ -35,7 +35,7 @@ namespace FormularioContacto
         private void btnEnviar_Click(object sender, EventArgs e)
         {
             //1. Validar el nombre
-            if(string.IsNullOrWhiteSpace(txtNombre.Text))
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
             {
                 MessageBox.Show("Por favor ingresa tu nombre.", "Validación",
                    MessageBoxButtons.OK,
@@ -44,27 +44,62 @@ namespace FormularioContacto
             }
             //2. Definir si desea respuesta
             List<string> Motivo = new List<string>();
-            if(radConsulta.Checked)
+            if (radConsulta.Checked)
             {
                 Motivo.Add(radConsulta.Text);
             }
-            if(radQueja.Checked)
+            if (radQueja.Checked)
             {
                 Motivo.Add(radQueja.Text);
             }
-            if(radSugerencia.Checked)
+            if (radSugerencia.Checked)
             {
                 Motivo.Add(radSugerencia.Text);
             }
 
-            if(Motivo.Count == 0) 
+            if (Motivo.Count == 0)
             {
                 MessageBox.Show("Por favor selecciona tu motivo de contacto.", "Advertencia",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Warning);
                 return;
-        }
+            }
             string respuesta = "";
+            if (chkRespuesta.Checked)
+            {
+                respuesta = chkRespuesta.Text;
+            }
+            else
+            {
+                respuesta = "No deseo recibir respuesta.";
+            }
+            string Confirmacion =
+                "MOTIVO DE CONTACTO\r\n" +
+                "============================\r\n" +
+                "Nombre: " + txtNombre.Text + "\r\n" +
+                "Motivo: " + string.Join(", ", Motivo) + "\r\n" +
+                "Desea respuesta: " + respuesta;
+            txtConf.Text = Confirmacion;
 
+        }
+
+        private void linkPolitica_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show(
+        "POLÍTICA DE PRIVACIDAD\n\n" +
+        "La información proporcionada en este formulario " +
+        "será utilizada únicamente para atender tu solicitud " +
+        "de contacto.\n\n" +
+        "Tus datos serán tratados de manera confidencial.",
+        "Política de privacidad",
+        MessageBoxButtons.OK,
+        MessageBoxIcon.Information
+              );
+        }
+
+        private void chkRespuesta_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
